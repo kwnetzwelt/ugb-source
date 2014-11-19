361a55922574d058263831853ddea2f90ea2784d
Kai Wegner <kai.wegner@exozet.com>
2014-11-19 15:08:18 +0100

    moved TransformSequence to Animation Namespace

--- UnityGameBase/animation/scripts/CTransformSequence.cs
+++ UnityGameBase/animation/scripts/CTransformSequence.cs
@@ -1,76 +1,78 @@
 using UnityEngine;
 using System.Collections;
 
-[ExecuteInEditMode()]
-public class CTransformSequence : MonoBehaviour
+namespace UGB.Animation
 {
-	public Vector3 mRotationStart;
-	public Vector3 mRotationEnd;
-	public Vector3 mPositionStart;
-	public Vector3 mPositionEnd;
-	
-	public AnimationCurve mEasing = new AnimationCurve( new Keyframe(0,0), new Keyframe(1,1));
-	
-	public float progress;
-	public float speed;
-	
-	float mReverse;
-	public bool isReverse
+	[ExecuteInEditMode()]
+	public class CTransformSequence : MonoBehaviour
 	{
-		get
+		public Vector3 mRotationStart;
+		public Vector3 mRotationEnd;
+		public Vector3 mPositionStart;
+		public Vector3 mPositionEnd;
+		
+		public AnimationCurve mEasing = new AnimationCurve( new Keyframe(0,0), new Keyframe(1,1));
+		
+		public float progress;
+		public float speed;
+		
+		float mReverse;
+		public bool isReverse
 		{
-			return mReverse != 1;
-		}
-	}
-	
-	[SerializeField]
-	public bool isPlaying
-	{
-		get;
-		private set;
-	}
-	
-	
-	public void Play()
-	{
-		Play(false);
-	}
-	
-	
-	public void Play(bool pReverse)
-	{
-		mReverse = (pReverse)? -1 : 1;
-		isPlaying = true;	
-	}
-	
-	
-	public void Pause()
-	{
-		isPlaying = false;
-	}
-	
-	
-	void Update ()
-	{
-		if(isPlaying)
-		{
-			if(progress > 1 || progress < 0)
+			get
 			{
-				// implement loop mode if wanted
-				isPlaying = false;
-				progress = Mathf.Clamp01( progress );
-			}else
-			{
-				progress += Time.deltaTime * mReverse * speed;
+				return mReverse != 1;
 			}
-			
-			this.transform.localPosition = Vector3.Lerp(mPositionStart,mPositionEnd, mEasing.Evaluate( progress ));
-			this.transform.localRotation = Quaternion.Lerp(
-				Quaternion.Euler( mRotationStart )
-				,Quaternion.Euler( mRotationEnd )
-				, progress);
-			
+		}
+		
+		[SerializeField]
+		public bool isPlaying
+		{
+			get;
+			private set;
+		}
+		
+		
+		public void Play()
+		{
+			Play(false);
+		}
+		
+		
+		public void Play(bool pReverse)
+		{
+			mReverse = (pReverse)? -1 : 1;
+			isPlaying = true;	
+		}
+		
+		
+		public void Pause()
+		{
+			isPlaying = false;
+		}
+		
+		
+		void Update ()
+		{
+			if(isPlaying)
+			{
+				if(progress > 1 || progress < 0)
+				{
+					// implement loop mode if wanted
+					isPlaying = false;
+					progress = Mathf.Clamp01( progress );
+				}else
+				{
+					progress += Time.deltaTime * mReverse * speed;
+				}
+				
+				this.transform.localPosition = Vector3.Lerp(mPositionStart,mPositionEnd, mEasing.Evaluate( progress ));
+				this.transform.localRotation = Quaternion.Lerp(
+					Quaternion.Euler( mRotationStart )
+					,Quaternion.Euler( mRotationEnd )
+					, progress);
+				
+			}
 		}
 	}
 }
-
--- UnityGameBase/animation/scripts/Editor/CTransformSequenceInspector.cs
+++ UnityGameBase/animation/scripts/Editor/CTransformSequenceInspector.cs
@@ -2,68 +2,71 @@
 using System.Collections;
 using UnityEditor;
 
-[CustomEditor(typeof(CTransformSequence))]
-public class CTransformSequenceInspector : Editor {
-	
-	bool mAdvancedEditing;
-	CTransformSequence mTarget;
-	public override void OnInspectorGUI()
-	{
-		CTransformSequence ts = target as CTransformSequence;
+namespace UGB.Animation
+{
+	[CustomEditor(typeof(CTransformSequence))]
+	public class CTransformSequenceInspector : UnityEditor.Editor {
 		
-		if(ts == null)
-			return;
-		
-		GUILayout.Label("Toggle Start / End Position to Edit");
-		GUILayout.BeginHorizontal();
-		bool tg = GUILayout.Toggle( ts.progress == 0, "Start","button");
-		if(tg)
+		bool mAdvancedEditing;
+		CTransformSequence mTarget;
+		public override void OnInspectorGUI()
 		{
-			ts.progress = 0;
-			ts.transform.localPosition = ts.mPositionStart;
-			ts.transform.localRotation = Quaternion.Euler( ts.mRotationStart );
-		}
-		tg = GUILayout.Toggle( ts.progress == 1,"End" ,"button");
-		
-		if(tg)
-		{
-			ts.progress = 1;
-			ts.transform.localPosition = ts.mPositionEnd;
-			ts.transform.localRotation = Quaternion.Euler( ts.mRotationEnd );
-		}
-		
-		
-		GUILayout.EndHorizontal();
-		
-		ts.mEasing = EditorGUILayout.CurveField("Easing", ts.mEasing);
-		
-		ts.speed = EditorGUILayout.Slider("Speed", ts.speed,0.01f,50.0f);
-		
-		mAdvancedEditing = EditorGUILayout.BeginToggleGroup("Advanced",mAdvancedEditing);
-		
-		DrawDefaultInspector();
-		
-		EditorGUILayout.EndToggleGroup();
-		
-	}
-	
-	void OnSceneGUI()
-	{
-		if(Application.isPlaying)
-			return;
-		
-		mTarget = target as CTransformSequence;
-		
-		if(mTarget.progress == 1)
-		{
-			mTarget.mRotationEnd = mTarget.transform.localRotation.eulerAngles;
-			mTarget.mPositionEnd = mTarget.transform.localPosition;
-		}else
-		{
-			mTarget.mRotationStart = mTarget.transform.localRotation.eulerAngles;
-			mTarget.mPositionStart = mTarget.transform.localPosition;
+			CTransformSequence ts = target as CTransformSequence;
+			
+			if(ts == null)
+				return;
+			
+			GUILayout.Label("Toggle Start / End Position to Edit");
+			GUILayout.BeginHorizontal();
+			bool tg = GUILayout.Toggle( ts.progress == 0, "Start","button");
+			if(tg)
+			{
+				ts.progress = 0;
+				ts.transform.localPosition = ts.mPositionStart;
+				ts.transform.localRotation = Quaternion.Euler( ts.mRotationStart );
+			}
+			tg = GUILayout.Toggle( ts.progress == 1,"End" ,"button");
+			
+			if(tg)
+			{
+				ts.progress = 1;
+				ts.transform.localPosition = ts.mPositionEnd;
+				ts.transform.localRotation = Quaternion.Euler( ts.mRotationEnd );
+			}
+			
+			
+			GUILayout.EndHorizontal();
+			
+			ts.mEasing = EditorGUILayout.CurveField("Easing", ts.mEasing);
+			
+			ts.speed = EditorGUILayout.Slider("Speed", ts.speed,0.01f,50.0f);
+			
+			mAdvancedEditing = EditorGUILayout.BeginToggleGroup("Advanced",mAdvancedEditing);
+			
+			DrawDefaultInspector();
+			
+			EditorGUILayout.EndToggleGroup();
+			
 		}
 		
-		EditorUtility.SetDirty(mTarget);
+		void OnSceneGUI()
+		{
+			if(Application.isPlaying)
+				return;
+			
+			mTarget = target as CTransformSequence;
+			
+			if(mTarget.progress == 1)
+			{
+				mTarget.mRotationEnd = mTarget.transform.localRotation.eulerAngles;
+				mTarget.mPositionEnd = mTarget.transform.localPosition;
+			}else
+			{
+				mTarget.mRotationStart = mTarget.transform.localRotation.eulerAngles;
+				mTarget.mPositionStart = mTarget.transform.localPosition;
+			}
+			
+			EditorUtility.SetDirty(mTarget);
+		}
 	}
 }
