db82fe4816b22ec190adef405c7ed9e7941a6578
Kai Wegner <kai.wegner@exozet.com>
2014-11-20 09:20:55 +0100

    moved InputDelegates to UGB.Input namespace andadded class documentation

--- UnityGameBase/Game/Input/InputDelegates.cs
+++ UnityGameBase/Game/Input/InputDelegates.cs
@@ -1,9 +1,21 @@
 using UnityEngine;
 using System.Collections;
-
-public class InputDelegates
+namespace UGB.Input
 {
-	public delegate void OnTouchEventDelegate(TouchInformation pTouchInfo);
-	public delegate void OnKeyMappingDelegate(string pKeyMappingName);
-}
+	/// <summary>
+	/// Contains all Input Delegates used by UGB.Input.GameInput
+	/// </summary>
+	public class InputDelegates
+	{
+		/// <summary>
+		/// Delegate used by UGB.Input.GameInput to notify other classes of a touch event. 
+		/// </summary>
 
+		public delegate void OnTouchEventDelegate(TouchInformation pTouchInfo);
+
+		/// <summary>
+		/// Delegate used by UGB.Input.GameInput to notify other classes of a key mapping event. 
+		/// </summary>
+		public delegate void OnKeyMappingDelegate(string pKeyMappingName);
+	}
+}
