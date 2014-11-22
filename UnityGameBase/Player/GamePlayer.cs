309d50caeff15e4eacb18c0091e29ff2743950d4
Marcel Ludwig <marclud8@gmail.com>
2014-11-22 09:34:14 +0100

    updated the GamePlayer class to have more options to handle e.g. a player change

--- UnityGameBase/Player/GamePlayer.cs
+++ UnityGameBase/Player/GamePlayer.cs
@@ -12,42 +12,99 @@ namespace UGB.Player
 		public delegate void PlayerStateChanged(PlayerState oldState,PlayerState newState);
 		public event PlayerStateChanged OnPlayerStateChanged;
 		
-		public PlayerState playerState
+		public PlayerState PlayerState
 		{
 			get;
 			private set;
 		}
 		
-		public Transform mPlayerPrefab;
-		
-		private Transform mPlayerInstance;
-		private IPlayerController mPlayerController;
-		
-		public Transform playerTransform
+		private Transform playerInstance;
+		private IPlayerController playerController;
+
+		/// <summary>
+		/// Returns the player transform. The controller transform
+		/// is preferred if set.
+		/// </summary>
+		/// <value>The player transform.</value>
+		public Transform PlayerTransform
 		{
-			get { if(mPlayerController != null) return mPlayerController.transform; else return null;}
+			get
+			{ 
+				if (playerController != null) 
+					return playerController.transform;
+				else if (playerInstance != null)
+					return playerInstance;
+				return null;
+			}
 		}
-		public IPlayerController playerController
+		public IPlayerController PlayerController
 		{
-			get { return mPlayerController; }
+			get { return playerController; }
 		}
-		
-		
-		
-		public void SetPlayerState(PlayerState pNewState)
+
+		public void SetPlayerState(PlayerState newState)
 		{
-			if(pNewState == playerState)
+			if (newState == PlayerState)
 				return;
-			Debug.Log("PlayerState: " + playerState + " > " + pNewState);
-		
-			PlayerState old = playerState;
-			playerState = pNewState;
-			if(OnPlayerStateChanged != null)
-				OnPlayerStateChanged(old,pNewState);
+
+			Debug.Log(string.Format("PlayerState changed: {0} > {1}", PlayerState, newState));
+
+			PlayerState oldState = PlayerState;
+			PlayerState = newState;
+			if (OnPlayerStateChanged != null)
+				OnPlayerStateChanged(oldState, newState);
 		}
-		public void SetPlayerController(IPlayerController pControllerInstance)
+
+		public void SetController(IPlayerController controller)
 		{
-			mPlayerController = pControllerInstance;
+			playerController = controller;
+		}
+
+		/// <summary>
+		/// Sets the new controller and destroys the previous player transform.
+		/// </summary>
+		/// <param name="controller">Controller.</param>
+		/// <param name="destroy">If set to <c>true</c> destroy.</param>
+		public void SetController(IPlayerController controller, bool destroy)
+		{
+			if (destroy)
+			{
+				RemoveController();
+			}
+			playerController = controller;
+		}
+
+		/// <summary>
+		/// Removes the controller and destroys the transform.
+		/// </summary>
+		public void RemoveController()
+		{
+			GameObject.Destroy(playerController.transform.gameObject);
+			playerController = null;
+		}
+
+		/// <summary>
+		/// Sets the players transform.
+		/// </summary>
+		/// <param name="player">PlayerInstance.</param>
+		public void SetTransform(Transform player)
+		{
+			playerInstance = player;
+		}
+
+		public void SetTransform(Transform player, bool destroy)
+		{
+			if (destroy)
+			{
+				RemoveTransform();
+			}
+			playerInstance = player;
+		}
+
+		public void RemoveTransform()
+		{
+			GameObject.Destroy(playerInstance.gameObject);
+			playerInstance = null;
 		}
 	}
 }
