00134429d0f8c33b6b5ae8dad040626c340deb17
Kai Wegner <kai.wegner@exozet.com>
2014-12-01 17:55:43 +0100

    Lazy Updates is now an abstract class. There were potential issues in the implementation. The documentation is now clear on what it does and how to use it.

--- UnityGameBase/Utils/LazyUpdates.cs
+++ UnityGameBase/Utils/LazyUpdates.cs
@@ -4,49 +4,62 @@ namespace UGB.Utils
 {
 	/// <summary>
 	/// A helper class, that provides bools which are toggled in a certain frequency. 
-	/// So you can run code when they are true or register to the <see cref="UGB.LazyUpdates::OnLazyUpdate"/> event.
+	/// So you can run code when they are true to ensure your code is not running every frame. 
+	/// Inherit from this class to us this functionality and implement a LazyUpdate method. 
+	/// IMPORTANT: This class can only be used to run code LESS-frequent than the update method runs. 
 	/// </summary>
-	public class LazyUpdates : GameComponent
+	public abstract class LazyUpdates : GameComponent
 	{
-		static LazyUpdates()
-		{
-			GameObject instance = new GameObject("_LazyUpdates");
-			DontDestroyOnLoad( instance );
-			instance.AddComponent<LazyUpdates>();
-		}
+
 		float lastLazyUpdateTime;
-		
+
+		/// <summary>
+		/// The frequency after which the next update is considered lazy. 
+		/// </summary>
 		public float LazyUpdateFrequency = 0.16f;
-		
+
+		/// <summary>
+		/// Gets a value indicating if the update is even. This returns true every second frame. 
+		/// </summary>
 		public static bool IsUpdateEven
 		{
 			get;
 			private set;
 		}
+
+		/// <summary>
+		/// Returns true during the Frame considered the lazy frame. 
+		/// </summary>
 		public static bool IsLazyUpdate
 		{
 			get;
 			private set;
 		}
-		
-		public event System.Action OnLazyUpdate;
-		
-		void FixedUpdate()
+
+		/// <summary>
+		/// Is called by this class automatically when the set time in UGB.Utils.LazyUpdates::LazyUpdateFrequency has passed. 
+		/// </summary>
+		protected abstract void LazyUpdate();
+
+		/// <summary>
+		/// You can override the update method to do your custom work. 
+		/// </summary>
+		protected virtual void Update()
 		{
 			IsUpdateEven = !IsUpdateEven;
 			
 			IsLazyUpdate = false;
 			
-			if((lastLazyUpdateTime + LazyUpdateFrequency) < Time.time)
+			if((lastLazyUpdateTime >= LazyUpdateFrequency))
 			{
-				lastLazyUpdateTime = Time.time;
+				lastLazyUpdateTime = 0;
 				IsLazyUpdate = true;
-				
-				if(OnLazyUpdate != null)
-				{
-					OnLazyUpdate();
-				}
+
+				LazyUpdate();
+
 			}
+
+			lastLazyUpdateTime += Time.deltaTime;
 		}
 	}
 }
