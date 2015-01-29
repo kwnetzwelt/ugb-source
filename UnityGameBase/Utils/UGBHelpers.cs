56c05e6803386d3db4ed1adb09b0ac43935d017c
Kai Wegner <kai.wegner@exozet.com>
2015-01-29 14:04:06 +0100

    added utility function to find a component in a parent.

--- UnityGameBase/Utils/UGBHelpers.cs
+++ UnityGameBase/Utils/UGBHelpers.cs
@@ -219,6 +219,22 @@ namespace UGB.Utils
 		}
 
 		#endregion
+
+        public static T FindInParents<T>(GameObject go) where T : Component
+        {
+            if (go == null) return null;
+            var comp = go.GetComponent<T>();
+            if (comp != null)
+                return comp;
+            
+            Transform t = go.transform.parent;
+            while (t != null && comp == null)
+            {
+                comp = t.gameObject.GetComponent<T>();
+                t = t.parent;
+            }
+            return comp;
+        }
 	}
 
 }
