ccafc59afeabed28678994b68b44f34baf8d5f04
Robert Stamm <robert.stamm@exozet.com>
2015-02-13 13:40:11 +0100

    added: PostProcessor and new Game-Instance-Initializer for using the UGB as SubModule

--- UnityGameBase/UGBSetup/scripts/Editor/Steps/CreateDefaultSceneStep.cs
+++ UnityGameBase/UGBSetup/scripts/Editor/Steps/CreateDefaultSceneStep.cs
@@ -6,54 +6,61 @@ using UGB;
 
 namespace UGBSetup
 {
-	internal class CreateDefaultSceneStep : UGBSetupStep
-	{
-		const string kScene = "Assets/scenes/default.unity";
-		public const string kGameRoot = "_GameRoot";
+    internal class CreateDefaultSceneStep : UGBSetupStep
+    {
+        const string kScene = "Assets/scenes/default.unity";
+        public const string kGameRoot = "_GameRoot";
 
-		public override string GetName ()
-		{
-			return "Create default scene";
-		}
+        public override string GetName()
+        {
+            return "Create default scene";
+        }
 
-		public override IEnumerator Run ()
-		{
-			if(!force)
-			{
-				FileInfo fi = new FileInfo( kScene );
-				if(fi.Exists)
-				{
-					Debug.Log("Default scene exists. Skipping. ");
-					yield break;
-				}
-			}
+        public override IEnumerator Run()
+        {
+            if(!force)
+            {
+                FileInfo fi = new FileInfo(kScene);
+                if(fi.Exists)
+                {
+                    Debug.Log("Default scene exists. Skipping. ");
+                    yield break;
+                }
+            }
 
-			if(EditorApplication.isUpdating) {
-				Debug.Log("Wait");
-				yield return 0;
-			}
-			EditorApplication.NewScene();
+            if(EditorApplication.isUpdating || EditorApplication.isCompiling)
+            {
+                Debug.Log("Wait");
+                yield return 0;
+            }
+            
+            EditorApplication.NewScene();
 
-			ClearScene();
+           
+            ClearScene();
+           
+            GameObject go = new GameObject();
+            go.name = kGameRoot;
+           
+            System.Type type = UGBSetupPostProcessor.GetGameInitializerType();
+            go.AddComponent(type);
+           
+            EditorApplication.SaveScene(kScene);
+        }
 
-			GameObject go = new GameObject();
-			go.name = kGameRoot;
-			go.AddComponent<Game>();
+        void ClearScene()
+        {
+            var gos = GameObject.FindObjectsOfType<GameObject>();
+            foreach(var go in gos)
+            {
+                if(go != null)
+                {
+                    GameObject.DestroyImmediate(go);
+                }
+            }
 
-			EditorApplication.SaveScene( kScene );
-		}
+        }
 
-		void ClearScene()
-		{
-			var gos = GameObject.FindObjectsOfType<GameObject>();
-			foreach(var go in gos)
-			{
-				if(go != null)
-					GameObject.DestroyImmediate(go);
-			}
-
-		}
-
-	}
+    }
 
 }
