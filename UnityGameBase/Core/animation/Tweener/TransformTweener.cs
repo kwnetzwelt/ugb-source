b51dd2b8ec794079ef6f442e001ac7efd69e06f4
Rainer Bernhardt <rainer.bernhardt@exozet.com>
2015-04-21 11:41:26 +0200

    Simple and extendable Tweening implementation

---
+++ UnityGameBase/Core/animation/Tweener/TransformTweener.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityGameBase.Core.Animation.Tweener
+{
+    [Serializable]
+    public class TransformTweener
+    {
+        private VectorTweener positionTweener = new VectorTweener();
+        private VectorTweener scaleTweener = new VectorTweener();
+        private QuaternionTweener rotationTweener = new QuaternionTweener();
+
+        public void TweenTo(Vector3 position, Vector3 scale, Quaternion rotation, float duration, Transform currentTransform)
+        {
+            positionTweener.TweenTo(currentTransform.position, position, duration);
+            scaleTweener.TweenTo(currentTransform.localScale, scale, duration);
+            rotationTweener.TweenTo(currentTransform.rotation, rotation, duration);
+        }
+
+        public bool IsTweening
+        {
+            get
+            {
+                return positionTweener.IsTweening || rotationTweener.IsTweening || scaleTweener.IsTweening;
+            }
+        }
+
+        public void SetValues(Transform transform)
+        {
+            transform.position = positionTweener.GetValue();
+            transform.localScale = scaleTweener.GetValue();
+            transform.rotation = rotationTweener.GetValue();
+        }
+    }
+}
