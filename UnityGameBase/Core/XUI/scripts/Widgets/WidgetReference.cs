9b720c178891af213ffa61af2622e37d4d58343c
Ingo Heßling <ingo.hessling>
2015-07-22 18:58:36 +0200

    * added widget reference

---
+++ UnityGameBase/Core/XUI/scripts/Widgets/WidgetReference.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityGameBase;
+
+
+namespace UnityGameBase.Core.XUI
+{
+	// Just a component to register widgets in the scene graph
+	public class WidgetReference : GameComponent<GameRoot>, IWidget 
+	{
+	}
+}
