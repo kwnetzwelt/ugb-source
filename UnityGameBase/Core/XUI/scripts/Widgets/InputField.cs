c7aecfb13676f989467a0c9f41fbf13564be0da4
Florian Siemer <florian.siemer@exozet.com>
2017-09-04 15:42:39 +0200

    * Added a mobile touch keyboard input finished event (that is not supported by Unity itself)

--- UnityGameBase/Core/XUI/scripts/Widgets/InputField.cs
+++ UnityGameBase/Core/XUI/scripts/Widgets/InputField.cs
@@ -5,6 +5,21 @@ namespace UnityGameBase.Core.XUI
 {
 	public class InputField : UnityEngine.UI.InputField, IWidget
 	{
-		
+		/// <summary>
+		/// Event. Fires when touch keyboard input for this input field is finished (e.g. by "Done" or "Cancel" button).
+		/// String: message
+		/// Bool: success (input has been finished via "Done").
+		/// </summary>
+		public event System.Action<string, bool> TouchKeyboardInputFinishedEvent;
+
+		#region MONO
+		void Update()
+		{
+			if(TouchKeyboardInputFinishedEvent != null && m_Keyboard != null && m_Keyboard.done)
+			{
+				TouchKeyboardInputFinishedEvent.Invoke(m_Keyboard.text, !m_Keyboard.wasCanceled);
+			}
+		}
+		#endregion
 	}
 }
