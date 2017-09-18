22f9683d3297089729f4b3c5aed3201c370e00e2
Sebastian Denzer <sebastian.denzer@gmail.com>
2017-09-18 15:10:20 +0200

    * invoke click event can be disabled via parameter (from Robi)

--- UnityGameBase/Core/XUI/scripts/Widgets/Toggle.cs
+++ UnityGameBase/Core/XUI/scripts/Widgets/Toggle.cs
@@ -3,50 +3,63 @@ using System.Collections;
 
 namespace UnityGameBase.Core.XUI
 {
-	public class Toggle : UnityEngine.UI.Toggle, IWidget
-	{
-		[Tooltip("Enable static custom button click event (ButtonClicked).")]
-		[SerializeField]
-		bool enableStaticEvent = true;
+    public class Toggle:UnityEngine.UI.Toggle, IWidget
+    {
+        [Tooltip("Enable static custom button click event (ButtonClicked).")]
+        [SerializeField]
+        bool enableStaticEvent = true;
 
-		[Tooltip("This id value can be used to differentiate between button types (e.g. for click sounds).\nIt is used for a static custom button click event (ButtonClicked).")]
-		[SerializeField]
-		int eventId = 0;
+        [Tooltip("This id value can be used to differentiate between button types (e.g. for click sounds).\nIt is used for a static custom button click event (ButtonClicked).")]
+        [SerializeField]
+        int eventId = 0;
 
-		/// <summary>
-		/// Occurs when button clicked. This static event can be used to get any button click, e.g. for playing button sounds.
-		/// Returns Toggle instance, eventId:int and toggleOn/off
-		/// </summary>
-		public static event System.Action<Toggle, int, bool> ToggleClicked;
+        /// <summary>
+        /// Occurs when button clicked. This static event can be used to get any button click, e.g. for playing button sounds.
+        /// Returns Toggle instance, eventId:int and toggleOn/off
+        /// </summary>
+        public static event System.Action<Toggle,int,bool> ToggleClicked;
 
-		/// <summary>
-		/// Temporarily disables the static event.
-		/// </summary>
-		bool tempDisableEvent = false;
+        /// <summary>
+        /// Temporarily disables the static event.
+        /// </summary>
+        bool tempDisableEvent = false;
 
-		protected override void Awake()
-		{
-			base.Awake();
-			onValueChanged.AddListener(OnToggleClicked);
-		}
+        protected override void Awake()
+        {
+            base.Awake();
+            onValueChanged.AddListener(OnToggleClicked);
+        }
 
-		/// <summary>
-		/// Use this method to enable/disable the toggle without sending a static event (to avoid
-		/// sounds being fired etc. when setting the toggle code-wise).
-		/// </summary>
-		public void SetToggleIsOn (bool on)
-		{
-			tempDisableEvent = true;
-			this.isOn = on;
-			tempDisableEvent = false;
-		}
+        /// <summary>
+        /// Use this method to enable/disable the toggle without sending a static event (to avoid
+        /// sounds being fired etc. when setting the toggle code-wise).
+        /// </summary>
+        public void SetToggleIsOn(bool on,bool invokeClickEvent = true)
+        {
+            //the idea is to overwrite the toggle event with a dummy event while changing the toggle value and then switch back to the old event
+            Toggle.ToggleEvent onValueChangedTemp = this.onValueChanged;
+            if(!invokeClickEvent)
+            {
+                this.onValueChanged = new Toggle.ToggleEvent();
+            }
 
-		void OnToggleClicked (bool toggled)
-		{
-			if (!tempDisableEvent && enabled && enableStaticEvent && ToggleClicked != null)
-			{
-				ToggleClicked(this, eventId, toggled);
-			}
-		}
-	}
+            tempDisableEvent = true;
+            this.isOn = on;
+            tempDisableEvent = false;
+
+            //switch back to the old event
+            if(!invokeClickEvent)
+            {
+                this.onValueChanged = onValueChangedTemp;
+            }
+        }
+
+        void OnToggleClicked(bool toggled)
+        {
+            if(!tempDisableEvent && enabled && enableStaticEvent && ToggleClicked != null)
+            {
+                ToggleClicked(this,eventId,toggled);
+            }
+        }
+    }
 }
