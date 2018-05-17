23b74ede27b5ef88ef7163df714d973ac64c71d4
Sebastian Denzer <sebastian.denzer@exozet.com>
2018-05-17 21:32:06 +0200

    * scrollrect: bool flag indicates if "is scrolling"

---
+++ UnityGameBase/Core/XUI/scripts/Widgets/ScrollRect.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UnityGameBase.Core.XUI
+{
+    public class ScrollRect : UnityEngine.UI.ScrollRect
+    {
+        public bool IsScrolling { get; private set; }
+
+        WaitForEndOfFrame waitForEndOfFrame;
+        Vector2 oldPosition;
+
+        protected override void Start()
+        {
+            base.Start();
+
+            waitForEndOfFrame = new WaitForEndOfFrame();
+        }
+
+        public override void OnScroll(PointerEventData data)
+        {
+            base.OnScroll(data);
+                  
+            IsScrolling = true;
+            ResetOnMovementFinish();
+        }
+
+        public override void OnBeginDrag(PointerEventData eventData)
+        {
+            base.OnBeginDrag(eventData);
+
+            IsScrolling = true;
+            StopAllCoroutines();
+        }
+
+        public override void OnEndDrag(PointerEventData eventData)
+        {
+            base.OnEndDrag(eventData);
+            ResetOnMovementFinish();
+        }
+
+        void ResetOnMovementFinish()
+        {
+            StopAllCoroutines();
+            StartCoroutine(ResetCoroutine());
+        }
+
+        IEnumerator ResetCoroutine()
+        {
+            do
+            {
+                oldPosition = normalizedPosition;
+                yield return waitForEndOfFrame;
+            }
+            while (!Vector2.Equals(oldPosition, normalizedPosition));
+
+            IsScrolling = false;
+        }
+    }
+}
