65efef8a7865171572bbfad352cf75408b5d6eff
Sebastian Denzer <sebastian.denzer@exozet.com>
2019-01-08 15:42:55 +0100

    * updated widget generator / prefab generator: unity 2018.3 deprecated methods

--- UnityGameBase/Core/XUI/scripts/Base/PrefabGenerator.cs
+++ UnityGameBase/Core/XUI/scripts/Base/PrefabGenerator.cs
@@ -19,6 +19,32 @@ namespace UnityGameBase.Core.XUI
         public void CloneAndSave(string path, GameObject root, bool includeInactive = false)
         {
 #if UNITY_EDITOR
+#if UNITY_2018_3_OR_NEWER
+            //create a copy to keep the child prefab references
+            GameObject copy = Instantiate(root) as GameObject;
+            copy.name = root.name;
+
+            //TODO maybe another point to do this, its not clear why it happened here... (for others except me)
+            copy.GetComponent<WidgetManager>().CreateCollection(includeInactive);
+
+            var success = false;
+
+            if (!File.Exists(path))
+                File.Delete(path);
+
+            GameObject parent = new GameObject("parent");
+            parent.AddComponent<XUIHierarchyViewer>();
+            copy.transform.SetParent(parent.transform);
+
+            DestroyImmediate(copy.GetComponent<PrefabGenerator>());
+                
+            //create a new prefab
+            UnityEditor.PrefabUtility.SaveAsPrefabAsset(parent, path, out success);
+            DestroyImmediate(parent);
+
+            Debug.Log("Saved prefab: " + root.name + " to " + path + " ... " + (success ? "SUCCESS" : "FAILED"));
+
+#else
             //create a copy to keep the child prefab references
             GameObject copy = Instantiate(root) as GameObject;
             copy.name = root.name;
@@ -58,6 +84,7 @@ namespace UnityGameBase.Core.XUI
 				
                 DestroyImmediate(parent);
             }
+#endif
 #endif // UNITY_EDITOR
         }
     }
